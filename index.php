16e10986f0112b21711a90dc3969afc99baf730b
krstn <kristine@klab.com>
2014-07-03 13:04:12 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,102 @@
+<? include 'template/header.php'; ?>
+
+        <div id="content">
+            <div class="sidebar">
+                <div class="group first">
+                    <div class="title">Categories</div>
+                    <ul class="items">
+                        <li>All</li>
+                        <li>Action</li>
+                        <li>Adventure</li>
+                        <li class="sel">Arcade</li>
+                        <li>Board</li>
+                        <li>Card</li>
+                        <li>Casino</li>
+                        <li>Dice</li>
+                        <li>Educational</li>
+                        <li>Music</li>
+                        <li>Family</li>
+                        <li>Puzzle</li>
+                        <li>Racing</li>
+                        <li>Role Playing</li>
+                        <li>Simulation</li>
+                        <li>Sports</li>
+                        <li>Strategy</li>
+                        <li>Trivia</li>
+                        <li>Word</li>
+                    </ul>
+                </div>
+                <div class="group">
+                    <div class="title">Page</div>
+                    <ul class="items">
+                        <li>All</li>
+                        <li>Action</li>
+                        <li>Adventure</li>
+                        <li class="sel">Arcade</li>
+                        <li>Board</li>
+                        <li>Card</li>
+                        <li>Casino</li>
+                        <li>Dice</li>
+                        <li>Educational</li>
+                        <li>Music</li>
+                        <li>Family</li>
+                        <li>Puzzle</li>
+                        <li>Racing</li>
+                        <li>Role Playing</li>
+                        <li>Simulation</li>
+                        <li>Sports</li>
+                        <li>Strategy</li>
+                        <li>Trivia</li>
+                        <li>Word</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="grid">
+                <div class="column">
+                    <div class="item">
+                        <img src="res/images/portrait.jpg" />
+                        <div class="title">Monument Valley</div>
+                        <div class="tags">Games, Puzzle, Splash, Minimalist</div> 
+                    </div>
+                    <div class="item">
+                        <img src="res/images/portrait.jpg" />
+                        <div class="title">Monument Valley</div>
+                        <div class="tags">Games, Puzzle, Splash, Minimalist</div>
+                    </div>
+                    <div class="item">
+                        <img src="res/images/landscape.jpg" />
+                        <div class="title">Monument Valley</div>
+                        <div class="tags">Games, Puzzle, Splash, Minimalist</div>
+                    </div>
+                    <div class="item">
+                        <img src="res/images/portrait.jpg" />
+                        <div class="title">Monument Valley</div>
+                        <div class="tags">Games, Puzzle, Splash, Minimalist</div> 
+                    </div>
+                    <div class="item">
+                        <img src="res/images/portrait.jpg" />
+                        <div class="title">Monument Valley</div>
+                        <div class="tags">Games, Puzzle, Splash, Minimalist</div>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="item">
+                        <img src="res/images/landscape.jpg" />
+                        <div class="title">Monument Valley</div>
+                        <div class="tags">Games, Puzzle, Splash, Minimalist</div> 
+                    </div>
+                    <div class="item">
+                        <img src="res/images/portrait.jpg" />
+                        <div class="title">Monument Valley</div>
+                        <div class="tags">Games, Puzzle, Splash, Minimalist</div>
+                    </div>
+                    <div class="item">
+                        <img src="res/images/portrait.jpg" />
+                        <div class="title">Monument Valley</div>
+                        <div class="tags">Games, Puzzle, Splash, Minimalist</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ template/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Kriativ</title>
+        <meta name="description" content="design">
+        <link rel="stylesheet" href="res/style/main.css">
+        <link href='http://fonts.googleapis.com/css?family=Lato:300,400' rel='stylesheet' type='text/css'>
+		<!--<link rel="icon" href="icons/logo.png">
+        <script type="text/javascript" src="scripts/events.js"></script>-->
+    </head>
+    
+    <body>
+            <div id="header">
+                <div class="nav">
+                    <a href="#" class="title">kriativ.me</a>
+                    <a href="#">Apps</a>
+                    <a href="#" class="sel">Games</a>
+                    <a href="#">All</a>
+                </div>
+                <input class="search" type="search" placeholder="Search..." />
+            </div>
+        
+        
